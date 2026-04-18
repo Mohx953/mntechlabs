dde232c9035f5539219360c998d5676c18e6651f
Mohx <mahamulekwanele@gmail.com>
2026-04-18 21:02:38 +0200

    Macbook parallax doesnt show when signed out- added this feature

--- src/components/macbook-scroll-demo.jsx
+++ src/components/macbook-scroll-demo.jsx
@@ -3,10 +3,13 @@ import { MacbookScroll } from "@/components/ui/macbook-scroll";
 import BackgroundBeamsDemo from "./background-beams-demo";
 import { BackgroundBeamsWithCollision } from "./ui/background-beams-with-collision-copy";
 import BackgroundRippleEffectDemo from "./background-ripple-effect-demo";
+import { Show } from "@clerk/react";
 
 export default function MacbookScrollDemo() {
   return (
- 
+  <>
+ <Show when="signed-out"></Show>
+ <Show when="signed-in">
     <div className="w-full overflow-hidden   dark:bg-[#0B0B0F]">
       
       <MacbookScroll
@@ -23,8 +26,9 @@ export default function MacbookScrollDemo() {
         src={`https://i.imghippo.com/files/ipeE7828rCc.png`}
         showGradient={false} />
     </div>
+    </Show>
     
-   
+   </>
   );
 }
 // MntLabs logo
